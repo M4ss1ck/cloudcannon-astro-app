1be8a00b8069f8391abe57fdb82829f1aefb85fb
Massick <4ndyraul@gmail.com>
2024-06-28 22:56:29 -0400

    move search and language selector to Navigation component

--- src/components/layouts/navigation.jsx
+++ src/components/layouts/navigation.jsx
@@ -1,10 +1,34 @@
 import { useEffect, useState } from "react";
 import navigation from "@data/navigation.json";
-import LanguageSelector from "../i18n/LanguageSelector";
-import Search from "../search/Search";
 
 export default function Navigation({ pageUrl }) {
   const [isSticky, setSticky] = useState(false);
+  const [lang, setLang] = useState("en");
+
+  const handleLanguageChange = (e) => {
+    const newLang = e.target.value;
+    const currentPath = window.location.pathname;
+
+    // Strip existing language prefix if present
+    const newPath =
+      currentPath.startsWith("/en/") || currentPath.startsWith("/es/")
+        ? currentPath.substring(currentPath.indexOf("/", 1))
+        : currentPath;
+
+    // Prepend new language prefix if != English
+    const updatedPath = newLang === "en" ? newPath : `/${newLang}${newPath}`;
+
+    // Navigate to the new path
+    window.location.href = updatedPath;
+  };
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    if (currentPath.startsWith("/es/")) {
+      setLang("es");
+    } else {
+      setLang("en");
+    }
+  }, []);
 
   const handleScroll = () => {
     setSticky(window.scrollY >= 70);
@@ -180,8 +204,50 @@ export default function Navigation({ pageUrl }) {
                   </li>
                 ))}
               </ul>
-              {/* <Search /> */}
-              <LanguageSelector />
+              <div className="search">
+                {/* <input type="text" placeholder="Search..." className="search-input" /> */}
+                <button
+                  className="text-primary bg-transparent border-1"
+                  style={{
+                    width: 40,
+                    height: 40,
+                    marginLeft: "auto",
+                    marginRight: 4,
+                  }}
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.25 14.2422C11.5637 14.2422 14.25 11.5559 14.25 8.24219C14.25 4.92848 11.5637 2.24219 8.25 2.24219C4.93629 2.24219 2.25 4.92848 2.25 8.24219C2.25 11.5559 4.93629 14.2422 8.25 14.2422Z"
+                      stroke="white"
+                      strokeWidth="1.2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M15.7469 15.7391L12.4844 12.4766"
+                      stroke="white"
+                      strokeWidth="1.2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <select
+                onChange={handleLanguageChange}
+                style={{ width: 80, height: 40 }}
+                className="bg-transparent border-1 text-white"
+                value={lang}
+              >
+                <option value="en">🇱🇷 ENG</option>
+                <option value="es">🇪🇸 ESP</option>
+              </select>
             </div>
           </div>
         </nav>
